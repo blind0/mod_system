eeec2385db04a3cfd482ff1de40c0a48a48d0eba
blind0 <ahapka1@gmail.com>
2023-01-12 20:19:43 +0500

    непонятные коммиты

--- bot.py
+++ bot.py
@@ -163,7 +163,7 @@ async def status_task() -> None:
     """
     Setup the game status task of the bot.
     """
-    statuses = ["with you!", "with Krypton!", "with humans!"]
+    statuses = ["забань дебила"]
     await bot.change_presence(activity=discord.Game(random.choice(statuses)))
 
 
